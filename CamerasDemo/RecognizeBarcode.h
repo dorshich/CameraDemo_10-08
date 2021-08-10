fbebcd286686cd0d319a2c9241e3f29b1ee8556f
dorshich <dor2802@gmail.com>
2021-08-10 10:41:58 +0300

    demo up_1008

---
+++ CamerasDemo/RecognizeBarcode.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class CRecognizeBarcode
+{
+public:
+	CRecognizeBarcode();
+	virtual ~CRecognizeBarcode();
+
+public:
+	CString m_sDynamSoftLicense;
+	HRESULT DynamSoft_PerformBarcodeRecognition(CString &fileName, std::vector<CString> &barcodeStrings, BOOL bOnlyQR = FALSE);
+};
+
+
+
